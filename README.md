8fd251f3b998655979594be525dee40417765708
Heakl <ahmed.heakl@ejust.edu.eg>
2024-09-05 01:52:11 +0400

    Added compile x86 using gcc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # asm2asm
 Assembly to assembly transpilation using LLMs
+
+
+Please download the AnghaBench and place it under the `data` folder.
+
+```bash
+git clone https://github.com/brenocfg/AnghaBench
+```
---
+++ scripts/compile_x86_gcc.py
@@ -0,0 +1,57 @@
+import glob
+import subprocess
+import multiprocessing
+import argparse
+
+OPT = ["O0", "O1", "O2", "O3"]  # Optimization states
+
+
+def compile(input_file, optimization_level="O0"):
+    base_output_file = input_file.replace(".c", "")
+    assert (
+        optimization_level in OPT
+    ), f"Invalid optimization level: {optimization_level}"
+    input_text = open(input_file).read()  # Read input file
+    if "/* Variables and functions */" in input_text:
+        # Exclude macro and types
+        input_text = input_text.split("/* Variables and functions */")[-1]
+        input_text = "\n\n".join(input_text.split("\n\n")[1:])  # Exclude variables
+        ##### begin of remove __attribute__
+        input_text = input_text.replace("__attribute__((used)) ", "")
+        ##### end of remove __attribute__
+    try:
+        asm_output = base_output_file + "_" + optimization_level + ".s"
+
+        # Compile the C program to object file
+        subprocess.run(
+            ["gcc", "-S", "-o", asm_output, input_file, "-" + optimization_level],
+            check=True,
+        )
+
+    except Exception as e:
+        print(f"Error in file {input_file}: {e}")
+        return
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Compile C files.")
+    parser.add_argument(
+        "--root",
+        required=True,
+        help="Root directory where AnghaBench/folder files are located.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    root = args.root
+    files = glob.glob(f"{root}/**/*.c", recursive=True)
+
+    with multiprocessing.Pool(4) as pool:
+        pool.map(compile, files)
+
+
+if __name__ == "__main__":
+    main()
