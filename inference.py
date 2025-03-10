2591e1d32de3a4b37d21185fd5d40d7beb0d13ed
Ahmed Heakl <52796111+ahmedheakl@users.noreply.github.com>
2025-03-10 12:01:05 +0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,6 +4,7 @@ from transformers import (
     AutoTokenizer,
 )
 
+
 model_name = "ahmedheakl/asm2asm-deepseek1.3b-xtokenizer-armv8"
 
 instruction = """<｜begin▁of▁sentence｜>You are a helpful coding assistant assistant on converting from x86 to ARM assembly.
@@ -17,11 +18,12 @@ Convert this x86 assembly into ARM
 {asm_arm}
 """
 
+
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="auto",
     torch_dtype=torch.bfloat16,
-    # attn_implementation="flash_attention_2", # uncomment if you have flash attention installed
+    attn_implementation="flash_attention_2", # uncomment if you have flash attention installed
 )
 
 model.config.use_cache = True
@@ -31,6 +33,8 @@ tokenizer = AutoTokenizer.from_pretrained(
     trust_remote_code=True,
 )
 
+
+
 def inference(asm_x86: str) -> str:
     prompt = instruction.format(asm_x86=asm_x86, asm_arm="")
     inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
@@ -38,9 +42,9 @@ def inference(asm_x86: str) -> str:
         **inputs,
         num_return_sequences=1,
         max_new_tokens=8000,
-        do_sample=True,
+        do_sample=False,
         num_beams=1,
-        top_p=0.9,
+        top_p=None,
         eos_token_id=tokenizer.eos_token_id,
         pad_token_id=tokenizer.pad_token_id,
     )
